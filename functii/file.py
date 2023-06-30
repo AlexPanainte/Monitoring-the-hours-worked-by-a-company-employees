95a3f512d7814b5d20f22a229bdbab2a34cda4b8
AlexPanainte <panainte_alex@yahoo.com>
2023-06-30 14:21:05 +0300

    structurare cod

--- functii/file.py
+++ functii/file.py
@@ -2,38 +2,63 @@ import os
 import shutil
 import csv
 import functii.constante as c
+import mysql.connector
 
-              
-class Fisiere_TXT():
 
-    # #2- Functie care citeste si returneaza continutul fisierelor din folderul intrari sub forma de lista de liste
-    def citeste_txt(self):
+class Fisiere():
+    def __init__(self):
+        self.connect_mysql=mysql.connector.connect(host="localhost",user="root",password="Afd3ufy250137@",database="users")
+        self.cursor_mysql=self.connect_mysql.cursor()
+
+
+class Fisiere_TXT(Fisiere):
+    
+    def read_txt(self):
         lista_continut=[]
+
         with open(f"{c.path}Poarta1.txt" ,"r") as file:
             reader=file.readlines()
+                        
             for line in reader:
-                linie_split=line.split(",")
+                linie_strip=line.strip("\n ;")
+                linie_split=linie_strip.split(",")
                 lista_continut.append(linie_split)
+
+            for i in lista_continut:
+                ID=i[0]
+                DATA=i[1] 
+                SENS=i[2]
+                self.cursor_mysql.execute(f"INSERT INTO ACCES_PORTI VALUES ('{ID}','{DATA}','{SENS}','Poarta1');")                
+                self.connect_mysql.commit()
+            print("Fisierul TXT a fost adaugat cu succes in baza de date")
 
     def move_and_rename_in_backup_TXT(self):
         os.rename(c.nume_initial_TXT,c.nume_final_TXT)
         shutil.move(c.nume_final_TXT,c.destination)
+        self.cursor_mysql.close()
+        self.connect_mysql.close()
 
-
-class Fisiere_CSV():
+class Fisiere_CSV(Fisiere):
     
     #1 Functie care citeste fisiere CSV
-    def citeste_csv(self):
-        with open(f"{c.path}Poarta2.csv" ,"r") as file:
-            reader=csv.reader(file)
-            for row in reader:
-                return(row)
+    def read_csv(self):
+        with open(f"{c.path}Poarta2.csv","r") as file:
+            fisier=csv.reader(file)
+            next(fisier)
+            for line in fisier:
+                ID=line[0]
+                DATA=line[1] 
+                SENS=line[2]
+                self.cursor_mysql.execute(f"INSERT INTO ACCES_PORTI VALUES ('{ID}','{DATA}','{SENS}','Poarta2');")                        
+                self.connect_mysql.commit()           
+            print("Fisierul CSV a fost adaugat cu succes in baza de date")
     
     def rename_and_move_in_backup_CSV(self):
         os.rename(c.nume_initial_CSV,c.nume_final_CSV)
         shutil.move(c.nume_final_CSV,c.destination)
 
-
+        self.cursor_mysql.close()
+        self.connect_mysql.close()
 
     
 
--- functii/users.py
+++ functii/users.py
@@ -1,12 +1,15 @@
 import mysql.connector
 import smtplib, ssl
 from email.message import EmailMessage
+from datetime import datetime
 
-connect=mysql.connector.connect(host="localhost",user="root",password="Afd3ufy250137@",database="users")
-cursor=connect.cursor()
+
 
 
 class User():
+    def __init__(self):
+        self.connect=mysql.connector.connect(host="localhost",user="root",password="Afd3ufy250137@",database="users")
+        self.cursor=self.connect.cursor()
    
     #1-Functie care inregistreaza utilizatori in baza de date registered_users
     def user_registration(self,nume,prenume,nume_companie,id_manager):
@@ -14,27 +17,28 @@ class User():
         self.prenume=prenume
         self.nume_companie=nume_companie
         self.id_manager=id_manager
-        cursor.execute(f"INSERT INTO REGISTERED_USERS VALUES (null,'{self.nume}','{self.prenume}','{self.nume_companie}','{self.id_manager}');")
-        connect.commit()
+        self.cursor.execute(f"INSERT INTO REGISTERED_USERS VALUES (null,'{self.nume}','{self.prenume}','{self.nume_companie}','{self.id_manager}');")
+        self.connect.commit()
         print("Utilizator inregistrat")
 
     #2- Functie care arata toti utilizatorii inregistrati in baza de date registered_users
     def show_users(self):
 
         select_users="SELECT * FROM REGISTERED_USERS;"
-        cursor.execute(select_users)
-        user=cursor.fetchall()
+        self.cursor.execute(select_users)
+        user=self.cursor.fetchall()
         for list in user:
             print(list)
+
     def send_email(self,angajati):
         sender_mail = "alexvasluigaming@gmail.com"
         email_password = "hdlkekhytlzipsqq"
-        email_reciever = "dragosarama595@gmail.com"
-
+        email_reciever = 'dragosarama595@gmail.com'
+        
         subject = "SALUT"
         body = "Următorii angajați nu au lucrat 8 ore:\n\n"
         for angajat in angajati:
-            body += f"ID: {angajat['ID']}, Ore: {angajat['Ore']}, Minute: {angajat['Minute']}\n"
+            body += f"ID: {angajat['ID']}, Ore: {angajat['Ore']}, Minute: {angajat['Minute']}, De recuperat: {angajat['Recuperare']}\n"
 
         em = EmailMessage()
         em['From'] = sender_mail
@@ -49,10 +53,67 @@ class User():
             server.quit()
         print("Email Sent")
 
-    
+    def calcul_ore(self):
+        self.cursor.execute("SELECT * FROM users.acces_porti  WHERE CAST(users.acces_porti.data as date) = curdate() ORDER BY ID_PERSOANA")
+        rows = self.cursor.fetchall()
+        index = []
+        angajat_cu_mai_putin_de_8ore_lucrate = []
 
+        for i in rows:
+            if i[0] not in index:
+                index.append(i[0])
 
+        for id in index:
+            ID = id
+            intrari = []
+            iesiri = []
 
-connect.close()
-cursor.close()
+            for i in rows:
+                if i[2] == 'in' and i[0] == ID:
+                    intrari.append(i[1])
+                elif i[2] == 'out' and i[0] == ID:
+                    iesiri.append(i[1])
+
+            if len(intrari) != len(iesiri):
+                raise ValueError("Numărul de intrări și ieșiri trebuie să fie același.")
+
+            numar_ore = 0
+            numar_minute = 0
+
+            for intrare, iesire in zip(intrari, iesiri):
+                try:
+                    intrare = datetime.strptime(intrare, '%Y-%m-%dT%H:%M:%S.%fZ')
+                    iesire = datetime.strptime(iesire, '%Y-%m-%dT%H:%M:%S.%fZ')
+
+                    diferenta = iesire - intrare
+                    diferenta_totala_secunde = diferenta.total_seconds()
+                    diferenta_ore = int(diferenta_totala_secunde) // 3600
+                    diferenta_minute = int(diferenta_totala_secunde % 3600) // 60
+
+                    numar_ore += diferenta_ore
+                    numar_minute += diferenta_minute
+                except ValueError:
+                    print("Formatul datelor de intrare sau ieșire este incorect.")
+            
+            total_minute = numar_ore * 60 + numar_minute
+            if total_minute < 480:  # 8 ore reprezintă 480 de minute
+                ore_ramase = (480 - total_minute) // 60
+                minute_ramase = (480 - total_minute) % 60
+
+                angajat = {
+                    'ID': ID,
+                    'Ore': numar_ore,
+                    'Minute': numar_minute,
+                    'Recuperare': f"{ore_ramase} ore și {minute_ramase} minute"
+                }
+                angajat_cu_mai_putin_de_8ore_lucrate.append(angajat)
+            else:
+                print(f"Timpul total de lucru pentru angajatul cu ID-ul {ID} este {numar_ore} ore și {numar_minute} minute.")
+            
+        if angajat_cu_mai_putin_de_8ore_lucrate:
+            user=User()
+            user.send_email(angajat_cu_mai_putin_de_8ore_lucrate)
+        
+        self.connect.close()
+        self.cursor.close()
 
